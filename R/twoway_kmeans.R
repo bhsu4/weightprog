71ba8740aa82dcb8d352fc0121b510b397bcb3ea
Benjamin Hsu <bhsu4@u.rochester.edu>
2017-12-08 12:40:15 -0500

    added more info for intro + description

--- R/twoway_kmeans.R
+++ R/twoway_kmeans.R
@@ -1,5 +1,5 @@
 twoway_kmeans <- function(xname, yname, kmax, xlabel, ylabel, guidecolor) {
-    data.km <- cbind(x1 = xname, x2 = yname)
+  data.km <- cbind(x1 = xname, x2 = yname)
   kclusts <- data.frame(kmax=1:kmax) %>% group_by(kmax) %>% do(kclust=kmeans(data.km, .$kmax))
   #do kmeans on data for each k-number of clusters
   assignments <- kclusts %>% group_by(kmax) %>% do(augment(.$kclust[[1]], data.km))
